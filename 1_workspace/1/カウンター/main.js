836dfb45d84458f45f864e242a58f9081e1c93f5
Taniguchihayato <thuchihayato1664@icloud.com>
2021-04-16 16:38:30 +0900

    カウント

--- 1_workspace/1/カウンター/main.js
+++ 1_workspace/1/カウンター/main.js
@@ -1,8 +1,18 @@
 const display = document.getElementById("display")
 const plusButton = document.getElementById("plus-button")
+const twice = document.getElementById("twice")
+const negative = document.getElementById("negative")
 let count = 0
 
 plusButton.onclick = function() {
   count += 1
+  display.textContent = count
+}
+twice.onclick = function() {
+  count *= 2
+  display.textContent = count
+}
+negative.onclick = function() {
+  count -= 1
   display.textContent = count
 }
