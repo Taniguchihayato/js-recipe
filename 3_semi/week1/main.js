e74b82c47d664362c32bb72aa6b5f67c57d989bd
Taniguchihayato <thuchihayato1664@icloud.com>
2021-04-17 16:24:41 +0900

    クイズ作りました

---
+++ 3_semi/week1/main.js
@@ -0,0 +1,7 @@
+const choice1 = document.getElementById("choice-1")
+const choice2 = document.getElementById("choice-2")
+const choice3 = document.getElementById("choice-3")
+const display = document.getElementById("display")
+choice1.onclick = function() {
+  display.textContent = display
+}
